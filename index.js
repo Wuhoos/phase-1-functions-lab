a28c34bc569eaafe591da04eadb85a1f6c9800bf
David <wjw3298@gmail.com>
2023-07-31 12:46:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks < 42){
+        return (42 - blocks)
+    }
+    else {
+        return (blocks - 42)
+    }
+}
+
+function distanceFromHqInFeet(blocks){
+  return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start < end){
+        return ((end - start)*264)
+    }
+    else {
+        return ((start-end)*264)
+    }
+}
+
+function calculatesFarePrice(start, end){
+    let distance = distanceTravelledInFeet(start, end)
+
+    if (distance <= 400){
+        return 0
+    }
+    else if (distance > 400 && distance < 2000){
+        return (distance- 400) * 0.02
+    }
+    else if (distance > 2000 && distance < 2500){
+        return 25
+    }
+    else if (distance >  2500){
+        return 'cannot travel that far'
+    }
+}
